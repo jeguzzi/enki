e270940d24fc4193e21ac4bad1a5bebf5c5079ff
Jérôme Guzzi <jerome@idsia.ch>
2025-10-25 18:59:34 +0200

    Added examples

---
+++ examples/python/video.py
@@ -0,0 +1,22 @@
+import pyenki
+
+from video import make_video
+
+world = pyenki.World()
+thymio = pyenki.Thymio2()
+thymio.left_wheel_target_speed = 10
+thymio.right_wheel_target_speed = -6
+thymio.set_led_circle(-1, 1.0)
+thymio.set_led_buttons(0, 1.0)
+thymio.set_led_buttons(2, 1.0)
+thymio.set_led_top(1.0, 0.0, 0.3)
+world.add_object(thymio)
+
+v = make_video(world,
+               time_step=0.033,
+               duration=20,
+               camera_position=(0, -20),
+               camera_altitude=20,
+               camera_pitch=-0.7)
+
+v.write_videofile('video.mp4', fps=30)
