c1363def6e68ba4faf63a19d3eb8fdb94b2748cd
Jérôme Guzzi <jerome@idsia.ch>
2025-11-24 15:52:22 +0100

    Fixed battery color

--- python/pyenki/viewer/renderer/shaders/thymio_body.fs
+++ python/pyenki/viewer/renderer/shaders/thymio_body.fs
@@ -15,6 +15,7 @@ struct LED {
   vec2 size;
 };
 
+const LED battery = LED(vec2(0.2773, 0.2373), vec2(0.0244, 0.0391));
 const LED top = LED(vec2(0.5, 0.5), vec2(1.0, 1.0));
 const LED bottom_left_0 = LED(vec2(0.6074, 0.8159), vec2(0.1133, 0.2939));
 const LED bottom_left_1 = LED(vec2(0.7309, 0.2163), vec2(0.1885, 0.1396));
@@ -61,7 +62,10 @@ void main(void) {
   vec4 led2_color = texture(led2, p);
   vec3 color = texture(u_texture, p).xyz * v_color;
   // vec4 led_color = vec4(0.0);
-  if (contained_in_led(top, p)) {
+  if (contained_in_led(battery, p)) {
+    vec4 led_color = compute_color(led0_color, vec4(1, 1, 0, 1));
+    color = mix(color, led_color.xyz, led_color.a);
+  } else if (contained_in_led(top, p)) {
     vec4 led_color = compute_color(led0_color, led_colors[0]);
     color = mix(color, led_color.xyz, led_color.a);
   }
